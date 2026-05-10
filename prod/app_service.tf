223cc776560d8c0507b66065fa1db3f3d671d033
Tadeáš Vintrlík <tadeas.vintrlik@posteo.net>
2026-05-10 13:48:22 +0200

    prod: fix typo in app_service.tf

--- prod/app_service.tf
+++ prod/app_service.tf
@@ -1,7 +1,7 @@
 resource "azurerm_service_plan" "asp" {
   name                = "asp-prod-demo"
-  resrouce_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resrouce_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "B1"
 }
